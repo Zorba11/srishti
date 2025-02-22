ed9df6a5d02fbd50820e76263a4a3a3b11ea7d92
Zorba11 <zorbathegreek111@gmail.com>
2025-02-22 12:53:56 -0500

    cinematico route protected

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,23 @@
-import { NextResponse } from "next/server";
-import config from "./config";
+import { NextResponse } from 'next/server';
+import config from './config';
 
-let clerkMiddleware: (arg0: (auth: any, req: any) => any) => { (arg0: any): any; new(): any; }, createRouteMatcher;
+let clerkMiddleware: (arg0: (auth: any, req: any) => any) => {
+    (arg0: any): any;
+    new (): any;
+  },
+  createRouteMatcher;
 
 if (config.auth.enabled) {
   try {
-    ({ clerkMiddleware, createRouteMatcher } = require("@clerk/nextjs/server"));
+    ({ clerkMiddleware, createRouteMatcher } = require('@clerk/nextjs/server'));
   } catch (error) {
-    console.warn("Clerk modules not available. Auth will be disabled.");
+    console.warn('Clerk modules not available. Auth will be disabled.');
     config.auth.enabled = false;
   }
 }
 
 const isProtectedRoute = config.auth.enabled
-  ? createRouteMatcher(["/dashboard(.*)"])
+  ? createRouteMatcher(['/dashboard(.*)', '/cinematico(.*)'])
   : () => false;
 
 export default function middleware(req: any) {
@@ -34,7 +38,7 @@ export default function middleware(req: any) {
 
 export const middlewareConfig = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
 };
